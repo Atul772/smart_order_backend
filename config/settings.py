4e30cbb77f107b7377a305a69c3d7f50db199e39
Atul772 <atulkumardec3@gmail.com>
2026-01-20 09:17:39 +0530

    fixed payment visibility in admin, and also put email info in env file

--- config/settings.py
+++ config/settings.py
@@ -177,10 +177,10 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "atulkumardec3@gmail.com"
-EMAIL_HOST_PASSWORD = "rcqdphicnkahtpwf"  # NOT normal Gmail password
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
-DEFAULT_FROM_EMAIL = "Smart Order <atulkumardec3@gmail.com>"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "amount",
+        "status",
+        "transaction_id",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "transaction_id",
+        "order__id",
+    )
+
+    ordering = ("-created_at",)
+
+    readonly_fields = (
+        "order",
+        "amount",
+        "status",
+        "transaction_id",
+        "created_at",
+        "updated_at",
+    )
